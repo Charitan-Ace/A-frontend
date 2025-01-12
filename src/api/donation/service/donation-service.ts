050750569f562ce5f6b1f618e8ff204c61f858ca
dophuonglinhh <s3926823@rmit.edu.vn>
2025-01-12 15:49:08 +0700

    update: set cookie for post donation

--- src/api/donation/service/donation-service.ts
+++ src/api/donation/service/donation-service.ts
@@ -1,9 +1,14 @@
 import { DONATION_URL } from "../constant";
+import { getCookie } from "typescript-cookie";
 import { DonationInput } from "../schema/donation-schema";
 import { postRequest } from "@/utils/http-request";
 
 const createDonation = async (data: Partial<DonationInput>) => {
-    const response = await postRequest<DonationInput>(DONATION_URL, data);
+    const response = await postRequest<DonationInput>(DONATION_URL, data, "include", {
+        "Content-Type": "application/json",
+        "Cookie": `charitan=${getCookie("charitan")}`,
+    });
+
     if (response.status !== 201) {
         throw new Error(`Failed to create donation. Status: ${response.status}`);
     }
