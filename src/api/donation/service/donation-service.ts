0d11259fbf4d401b82706806c1561a9618aec592
dophuonglinhh <s3926823@rmit.edu.vn>
2025-01-13 16:38:21 +0700

    update: update form donate as guest

--- src/api/donation/service/donation-service.ts
+++ src/api/donation/service/donation-service.ts
@@ -1,13 +1,18 @@
-import { DONATIONS_URL } from "../constant";
+// import { DONATIONS_URL } from "../constant";
 import { DonationInput } from "../schema/donation-schema";
 import { postRequest } from "@/utils/http-request";
 
 const createDonation = async (data: Partial<DonationInput>) => {
+  const DONATIONS_URL = "https://gateway.tail03350e.ts.net/donation"
+  const COOKIE = "eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMjU2R0NNIn0.Mc7Na-Lf_M4gTAROVT3QJJUrsorR5RYM97fgzNoi3ZCkB9fyGjc7gDAeywu9-86VH_l4uF3cuJFkvHBrKddMuHIvy-V5mziDbEPD_0Ox3SMbCmDPXoeWpt00lHOLIoeIGk0uHgVGtvAuD52DZwU7h3hMOoCCyswGJ4K51oLUgiR8jaYthwzkd9m7p7j8NsD3odeWd3Zto3o2nPnr_TZotJKbD0nQ4iZ_bOe7rQE8pwXIoZmFNofhNKz1o4uoap1LhYkAUPJUL2eU39R1wGy8ogrYDRR-WJk8yUBuitYOPHSivBnHNtV-ooM3CUB_hZsy90b2qVd6F9afEjboU_cV9Q.uPitM6mvYhXk3VgP.mi_yKMXJk1sztj_IK9p2kcf87Kgh4Xtq1R8bncBb9MWNBjt44u7haAMb61JYfACrefPm36dEER3ykXnyvv3rJKHor0N-VG6HyKdfGwmL_K0dxFGK4moa6rRvU5vmIjXIRVkLXwAmo9Qoxq6vllaM881CTkku9SwMIjwa66QcO2j8SEcEvqQRSSjz9OG0Rahq1vJEpx0iQgyEiu6ucUdxPSeyzNkMcrPjDxJ6lQXrtdaO4W3e1ngohefJO3oAcE9yvV6p8vjCIL67eoM_fetuXSsf5DvjPVE8sNfixSip_0zsexRGcLEYkjlGchoQItA--BX6y04w2HfOG7tYzhcfS8bAa-VgffgvTFUlvuhbLhE9blCOs4jqktb2y2mxMw.HqqOWyrRaAtuQlEKGaQy_w"
   const response = await postRequest<DonationInput>(
     DONATIONS_URL,
     data,
     "include",
-    { "Content-Type": "application/json" }
+    {
+      "Content-Type": "application/json",
+      Cookie: `charitan=${COOKIE}`,
+    }
   );
 
   if (response.status !== 201) {
--- src/components/donate-form/hooks/useDonateForm.ts
+++ src/components/donate-form/hooks/useDonateForm.ts
@@ -3,30 +3,63 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "react-toastify";
 import { DonationInput, donationSchema } from "@/api/donation/schema/donation-schema";
 import { createDonation } from "@/api/donation/service/donation-service";
+import { truncate } from "fs/promises";
 
 const useDonateForm = (projectId: string, onClose: () => void) => {
+  const successUrl = "http://localhost:3000/success";
+  const cancelUrl = "http://localhost:3000/cancel";
+
   const {
     register,
     handleSubmit,
-    reset,
+    // reset,
     formState: { errors },
   } = useForm<DonationInput>({
     resolver: zodResolver(donationSchema),
     defaultValues: { projectId },
   });
 
+  const payloadDoner = (data: DonationInput) => {
+    const { amount, message, projectId } = data;
+
+    const payload = {
+      amount: amount,
+      message: message,
+      projectId: projectId,
+      successUrl: successUrl,
+      cancelUrl: cancelUrl,
+    }
+
+    return payload;
+  }
+
+  const payloadGuest = (data: DonationInput) => {
+    const { amount, message, projectId } = data;
+
+    const payload = {
+      amount: amount,
+      message: message,
+      projectId: projectId,
+      successUrl: successUrl,
+      cancelUrl: cancelUrl,
+      email: "test@gmail.com",
+      firstName: "Linh",
+      lastName: "Do",
+      address: "My Home"
+    }
+    return payload;
+  }
+
   const onSubmit = async (data: DonationInput) => {
     try {
-      const { amount, message, projectId } = data;
-      const payload: Partial<DonationInput> = {
-        amount,
-        message,
-        projectId,
-      };
+      const isGuest = true
+      const payload = isGuest ? payloadGuest(data):  payloadDoner(data)
 
-      await createDonation(payload);
-      toast.success("Donate successfully. Thank you for your donation!");
-      onClose();
+      var body = await createDonation(payload);
+      window.location.href = body.redirectUrl
+
+      // toast.success("Donate successfully. Thank you for your donation!");
+      // onClose();
       // reset();
     } catch (error) {
       console.error("Donation failed:", error);
